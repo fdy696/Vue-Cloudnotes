d8e66c30fa56f58eacaff15932bcbbf3ce8f9845
fdy696 <3456283371.com>
2018-09-20 09:32:12 +0800

    '配置路由'

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import NotebookList from '@/components/NotebookList'
+import NoteDetail from '@/components/NoteDetail'
+import Trash from '@/components/Trash'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      path: '/login',
+      name: 'Login',
+      component: Login
+    }, {
+      path: '/notebooks',
+      name: 'Notebooks',
+      component: NotebookList
+    }, {
+      path: '/note',
+      name: 'NoteDetail',
+      component: NoteDetail
+    }, {
+      path: '/trash',
+      name: 'Trash',
+      component: Trash
+    },
   ]
 })
